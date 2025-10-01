0527c3ec92440b5e7ad51a47af51fd0a5039cf4e
Bernard Zulu <bernardzulu23@users.noreply.github.com>
2025-10-02 00:08:43 +0200

    trigger redeploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,7 @@
-
-//this file I want to use the supabase api for my portfolio website
 export const environment = {
-  production: false,
+  production: true,
   supabase: {
     url: 'https://kckgihiaaoioqlrjywpi.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja2dpaGlhYW9pb3Fscmp5d3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTIwNjAsImV4cCI6MjA3NDIyODA2MH0.oeJ7XV-rzYj2Y5M3wlo0itUD8FtFGEh0AldWyBZAMSY'
   }
 };
-
-
