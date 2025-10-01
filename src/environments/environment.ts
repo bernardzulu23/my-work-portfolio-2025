1b152f84c79dd1cdb956f0aa9363fccd5096a4e0
Bernard Zulu <bernardzulu23@users.noreply.github.com>
2025-10-01 23:48:15 +0200

    fix: add environment file to repository

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://kckgihiaaoioqlrjywpi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja2dpaGlhYW9pb3Fscmp5d3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTIwNjAsImV4cCI6MjA3NDIyODA2MH0.oeJ7XV-rzYj2Y5M3wlo0itUD8FtFGEh0AldWyBZAMSY'
+  }
+};
